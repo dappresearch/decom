07e6c8131559bcaaf2a3ff9b0e10031850b1970f
decentralmind <decentralmind@gmail.com>
2024-09-24 18:54:46 +0545

    update

--- src/PenguStore.sol
+++ src/PenguStore.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Deco is Ownable {
+// Need to figure out what to sell
+// This is important
+
+// Shippin cost for the cap
+contract PenguStore is Ownable {
 
     uint32 public totalStock;
 
@@ -15,15 +19,18 @@ contract Deco is Ownable {
     // single price
     uint256 public price;
 
+    uint256 totalPayment;
+
+    uint256 totalWithdraw;
+
     // revenue generted after fulfilling shipping
-    uint256 revenueAfterShipping;
+    uint256 amountAfterShipping;
 
     constructor(address owner) Ownable(owner) {
 
     }
-
+    
     struct Order {
-        string trackingNo;
         string shippingAddr;
         uint256 quantity;
         uint256 amount;
@@ -34,12 +41,9 @@ contract Deco is Ownable {
 
     // store buyer orders
     mapping(address => uint256[]) buyersOrder;
-
-    mapping(address => uint256) payments;
-
     mapping (uint32 => Order) orders;
 
-    uint256 totalPayment;
+    mapping(address => uint256) private _payments;
 
     function setStock(uint32 newTotalStock) external onlyOwner {
         totalStock = newTotalStock;
@@ -53,50 +57,56 @@ contract Deco is Ownable {
         shippingCost = newShippingCost;
     }
 
+    // need to properly calculate shipping cost
+    function totalCost(uint8 quantity) public view returns (uint256) {
+        return ((price * quantity) + shippingCost);
+    }
+
+    // Need to figure out accurate cost of the shipping
     function purchase(uint8 quantity, string memory destination) external payable {
         require(quantity > 0 && quantity <= totalStock, "Invalid quantity");
-        require(msg.value == price * quantity, "Incorrect payment amount");
+        require(msg.value == totalCost(quantity), "Incorrect payment amount");
 
         uint256 amount = msg.value;
-        Order storage order = orders[orderNo++];
+        Order storage order = orders[orderNo];
         order.shippingAddr = destination;
         order.quantity = quantity;
         order.amount = amount;
         order.buyerAddr = msg.sender;
 
         // record the amount of payment by the buyer
-        payments[msg.sender] += amount;
+        _payments[msg.sender] += amount;
         buyersOrder[msg.sender].push(orderNo);
 
         // overflow not possible, quantity <= stock, already checked.
         unchecked {
             totalStock -= quantity;
         }
+
+        orderNo++;
     }
-    
-    function processShipment(uint32 _orderNum, string memory trackingNo) external onlyOwner {
+
+    //Need to track the buyer balance
+    function processShipment(uint32 _orderNum) external onlyOwner {
         Order storage order = orders[_orderNum];
         require(!order.isShipped, "Already Shipped");
-        require(bytes(order.trackingNo).length == 0, "Tracking Number already set");
-
         order.isShipped = true;
-        //  check the tracking number from the system.
-        order.trackingNo = trackingNo;
-        revenueAfterShipping += order.amount;
+
+        amountAfterShipping += order.amount;
     }
 
-    // Update tracking number incase of problem
-    function updateTrackingNo(uint32 _orderNo, string memory trackingNo) external onlyOwner {
-        Order storage order = orders[_orderNo];
-        require(order.isShipped, "Use UpdateShipMent method");
-        order.trackingNo = trackingNo;
-    }
-
-    // Only able to withdraw for shipped items.
+    // Need to process multiple payment
+    // This is important.
+    // Only able to withdraw for shipped items
     function withdraw(address receiver) external onlyOwner {
-        require(revenueAfterShipping > 0, "Insufficient Revenue");
-        payable(receiver).transfer(revenueAfterShipping);
-        revenueAfterShipping = 0;
+        uint256 withdrawAmount = amountAfterShipping;
+
+        require(amountAfterShipping > 0, "Insufficient Revenue");
+
+        totalWithdraw += amountAfterShipping;
+        amountAfterShipping = 0;
+
+        payable(receiver).transfer(withdrawAmount);
     }
 
     // set cancel multiple orderNo
@@ -104,7 +114,7 @@ contract Deco is Ownable {
     function setCancelAndRefund(uint32 _orderNo) external onlyOwner {
         Order storage order = orders[_orderNo];
         require(!order.isShipped, "Already Shipped");
-        require(order.amount > 0, "Buyers need to pay");
+        require(order.amount > 0 && _payments[msg.sender] >= order.amount, "Buyers need to pay");
         order.cancelAndRefund = true;
     }
 
@@ -114,7 +124,7 @@ contract Deco is Ownable {
         for(uint8 i=0; i<_ordersNo.length; i++) {
             Order storage order = orders[_ordersNo[i]];
             require(!order.isShipped, "Already Shipped");
-            require(order.amount > 0, "Buyers need to pay");
+            require(order.amount > 0 && _payments[msg.sender] >= order.amount, "Buyers need to pay");
             order.cancelAndRefund = true;
         }
     }
@@ -123,70 +133,32 @@ contract Deco is Ownable {
     // It needs reentrance guard
     function collectRefund(uint32 _orderNo) external {
         Order memory order = orders[_orderNo];
+
+        // buyer shoule be caller
+        require(msg.sender == order.buyerAddr, "Caller should be buyer");
+        
+        // order should not be shipped
+        // orderNumber should be to set to cancelAndRefund
         require(!order.isShipped && order.cancelAndRefund, "Invalid refund");
-        require(msg.sender == order.buyerAddr);
-        totalPayment -= order.amount;
+
+        // must have valid payment
+        // totalPayment is not necessary important here
+        require(_payments[msg.sender] >= order.amount && totalPayment >= order.amount, "Incorrect payment request");
+
         uint256 amount = order.amount;
+        
+        unchecked {
+            totalPayment -= order.amount;
+            _payments[msg.sender] -= order.amount;
+        }
+        
         order.amount = 0;
+        delete orders[_orderNo];
+
+        // remove the payment
         payable(msg.sender).transfer(amount);
     }
-    
 }
-
-
-    // Need reentrant guard.
-    // Do not make too much complex.
-    // function refund(uint32 _orderNum) external onlyOwner {
-    //     Order memory order = orders[_orderNum];
-        
-    //     address currentBuyer = order.buyerAddr;
-
-    //     uint256 buyerPayment = payments[currentBuyer];
-
-    //     require(!order.isShipped && !order.cancelAndRefund, "Invalid refund");
-
-    //     require(buyerPayment > 0 && buyerPayment >= order.quantity * price, "Insufficent buyers amount");
-
-    //     payments[currentBuyer] -= 
-    //     // Function to transfer Ether from this contract to address from input
-    //     //(bool success,) = _to.call{value: _amount}("");
-    // }
-
-
-    // // orders => address mapping
-    // // addre
-    // // Basically good idea to group the orders and link that.
-    // function purchase(uint8 quantity, string memory destination) external payable {
-    //     require(quantity > 0 && quantity <= stock, "Invalid quantity");
-
-    //     uint256 amount = msg.value;
-
-    //     require(amount == price * quantity, "Incorrect payment amount");
-
-    //     // overflow not possible, quantity <= stock, already checked.
-    //     unchecked {
-    //         stock -= quantity;
-    //     }
-
-    //     // record the amount of payment by the buyer
-    //     paid[msg.sender] += amount;
-    // }
-
-    // // Set tracking number from the email address.
-    // function updateShipment(uint256 tokenId, string memory trackingNo) external onlyOwner {
-    //         ShipDetails storage sd = shipping[tokenId];
-
-    //         require(!sd.isShipped, "Already Shipped");
-
-    //         sd.isShipped = true;
-    //         //  check the tracking number from the system.
-    //         sd.trackingNo = trackingNo;
-    // }
-
-    // function setPrice(uint256 amount) external onlyOwner {
-    //     price = amount;
-    // }
-
 // How to contact a buyer if there is a shipping problem?
     // function uri(uint256 tokenId) override public view returns (string memory) {
     //     return string(
