4a005c19bdecd52578a2e82af38c4af6a133bca0
dappresearch <dappresearch@proton.me>
2024-11-17 17:27:07 +0545

    Move MockAggregratorV3Interface to mocks folder

--- test/DeCom.t.sol
+++ test/DeCom.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {DeCom, Ownable, PriceFeedV3, IError, Order, Status} from "../src/DeCom.sol";
-import {MockAggregratorV3Interface} from "../src/MockAggregratorV3Interface.sol";
+import {MockAggregratorV3Interface} from "../src/mocks/MockAggregratorV3Interface.sol";
 
 contract PenguStoreTest is Test {
     DeCom public decom;
